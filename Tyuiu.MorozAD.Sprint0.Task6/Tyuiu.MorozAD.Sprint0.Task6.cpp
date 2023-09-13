e7a0cf305d61fee48a5bb47181abe07cb36e5895
Tyuiu.MorozAD@outlook.com <ggllaa5566q@gmail.com>
2023-09-13 17:47:46 +0500

    Task6

---
+++ Tyuiu.MorozAD.Sprint0.Task6/Tyuiu.MorozAD.Sprint0.Task6.cpp
@@ -0,0 +1,19 @@
+﻿// Tyuiu.MorozAD.Sprint0.Task6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Введеите x:";
+    int x;
+    cin >> x;
+    cout << "Введеите y:";
+    int y;
+    cin >> y;
+    int s = (x * 8) / y * 2;
+    cout << "Ответ : "<<s;
+}
+
